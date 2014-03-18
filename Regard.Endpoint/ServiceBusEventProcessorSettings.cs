44269742398076f2240d8734df5c6335dc3c548f
Kevin Boyle <kevin.boyle@red-gate.com>
2014-03-18 13:42:41 +0000

    Make the settings configurable so I can get this building on TeamCity.
    Not an ideal, or long term solution, but it will do until we get a true local mode

--- Regard.Endpoint/ServiceBusEventProcessorSettings.cs
+++ Regard.Endpoint/ServiceBusEventProcessorSettings.cs
@@ -1,16 +1,35 @@
-﻿namespace Regard.Endpoint
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace Regard.Endpoint
 {
     internal class ServiceBusEventProcessorSettings : IServiceBusEventProcessorSettings
     {
         public string ServiceBusConnectionString
         {
-            get
-            {
-            }
+            get { return (string) GetAzureSettingsObject()["ServiceBusConnectionString"]; }
         }
 
         public string AnalyticsTopicName
         {
+            get { return (string) GetAzureSettingsObject()["ServiceBusTopicName"]; }
+        }
+
+        private JObject GetAzureSettingsObject()
+        {
+            return JObject.Parse(GetEmbeddedAzureSettings());
+        }
+
+        private string GetEmbeddedAzureSettings()
+        {
+            Stream manifestResourceStream = Assembly.GetCallingAssembly().GetManifestResourceStream("Regard.Endpoint.AzureSettings.AzureSettings.json");
+
+            if (manifestResourceStream == null)
+                throw new InvalidOperationException("Unable to find an embedded Azure Settings file");
+
+            return new StreamReader(manifestResourceStream).ReadToEnd();
         }
     }
 }
