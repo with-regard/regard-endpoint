065443d1a56ec4d54c8e077fa48d43d4731d925e
Kevin Boyle <kevin.boyle@red-gate.com>
2014-03-18 17:09:45 +0000

    Taken from http://msdn.microsoft.com/en-us/library/microsoft.build.tasks.msbuild.toolsversion(v=vs.121).aspx

--- default.ps1
+++ default.ps1
@@ -13,14 +13,14 @@ task compile {
     "Compiling"
     "   Regard.Endpoint.sln"
     
-    exec { msbuild $base_dir\Regard.Endpoint.sln /p:Configuration=$config /p:VisualStudioVersion=10.0 }
+    exec { msbuild $base_dir\Regard.Endpoint.sln /p:Configuration=$config /tv:4.0 }
 }
 
 task package -depends compile {
     "Packaging"
     "   Regard.Endpoint.sln"
 
-    exec { msbuild $base_dir\Regard.Endpoint\Regard.Endpoint.csproj /t:Package /p:Configuration=$config /p:VisualStudioVersion=10.0  }
+    exec { msbuild $base_dir\Regard.Endpoint\Regard.Endpoint.csproj /t:Package /p:Configuration=$config /tv:4.0  }
 }
 
 function Get-ProgramFiles
