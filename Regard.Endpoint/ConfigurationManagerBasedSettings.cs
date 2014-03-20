0e4f5ca114fddaaaef9a2115ed228a339940eef9
Kevin Boyle <kevin.boyle@red-gate.com>
2014-03-20 12:02:42 +0000

    few method extracts to aid debugging

--- Regard.Endpoint/ConfigurationManagerBasedSettings.cs
+++ Regard.Endpoint/ConfigurationManagerBasedSettings.cs
@@ -4,17 +4,23 @@ namespace Regard.Endpoint
 {
     internal class ConfigurationManagerBasedSettings : IServiceBusEventProcessorSettings
     {
+        private readonly string m_ServiceBusConnectionString;
+        private readonly string m_AnalyticsTopicName;
+
+        public ConfigurationManagerBasedSettings()
+        {
+            m_ServiceBusConnectionString = ConfigurationManager.AppSettings.Get("regard-service-bus-conenction-string");
+            m_AnalyticsTopicName = ConfigurationManager.AppSettings.Get("regard-service-bus-topic-name");
+        }
+
         public string ServiceBusConnectionString
         {
-            get
-            {
-                return ConfigurationManager.AppSettings.Get("regard-service-bus-conenction-string");
-            }
+            get { return m_ServiceBusConnectionString; }
         }
 
         public string AnalyticsTopicName
         {
-            get { return ConfigurationManager.AppSettings.Get("regard-service-bus-topic-name"); }
+            get { return m_AnalyticsTopicName; }
         }
     }
 }
