cd1fd67e385c8972a2c8530c075249405e7a9828
Kevin Boyle <kevin.boyle@outlook.com>
2014-05-12 18:25:58 +0100

    reorder

--- Regard.Endpoint/Startup.cs
+++ Regard.Endpoint/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using Microsoft.Owin.Cors;
 using Ninject;
 using Owin;
 using WebApiContrib.IoC.Ninject;
@@ -11,21 +12,19 @@ namespace Regard.Endpoint
         {
             // Configure for attribute routes
             var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.DependencyResolver = new NinjectResolver(new StandardKernel(new NinjectSettings
+            {
+                InjectNonPublic = true
+            }, new EndpointModule()));
             httpConfiguration.MapHttpAttributeRoutes();
 
-            httpConfiguration.DependencyResolver = new NinjectResolver(new StandardKernel(new NinjectSettings
-                {
-                    InjectNonPublic = true
-                }, new EndpointModule()));
-
-            app.UseWebApi(httpConfiguration);
-
-            // New code:
-            app.Run(async context =>
-                          {
-                              context.Response.ContentType = "text/plain";
-                              await context.Response.WriteAsync("Hello, world. Again.");
-                          });
+            app.UseCors(CorsOptions.AllowAll)
+                .UseWebApi(httpConfiguration)
+                .Run(async context =>
+                           {
+                               context.Response.ContentType = "text/plain";
+                               await context.Response.WriteAsync("Hello, world. Again.");
+                           });
         }
     }
 }
