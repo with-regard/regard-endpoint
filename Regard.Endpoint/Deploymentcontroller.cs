e65c441cdabedbbdca893740e749c94b819260c3
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-03 09:27:19 +0100

    Version the assembly for cheap deployment checking

---
+++ Regard.Endpoint/Deploymentcontroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+
+namespace Regard.Endpoint
+{
+    public class Deploymentcontroller : ApiController
+    {
+        [HttpGet, Route("deployment")]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK)
+                   {
+                       Content = new StringContent(Assembly.GetExecutingAssembly().GetName().Version.ToString())
+                   };
+        }
+    }
+}
--- default.ps1
+++ default.ps1
@@ -5,6 +5,7 @@ properties {
     $package_dir = "$base_dir\packages"
     $framework_dir =  (Get-ProgramFiles) + "\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0"
     $config = "release"
+    $build_number = if ($env:build_number -eq $null) { "0.0.0.1" } else { $env:build_number }
 }
 
 task default -depends package
@@ -22,7 +23,11 @@ task clean {
 	Exec { msbuild $base_dir\Regard.Endpoint.sln /t:Clean /p:Configuration=$config } 
 }
 
-task compile -depends clean {
+task version {
+	Update-AssemblyInfofiles $build_number
+}
+
+task compile -depends clean, version {
     "Compiling"
     "   Regard.Endpoint.sln"
     
@@ -42,6 +47,31 @@ task package -depends test {
     exec { msbuild $base_dir\Regard.Endpoint\Regard.Endpoint.csproj /t:Package /p:Configuration=$config /p:PackageLocation="$build_dir\package.zip" /tv:4.0  }
 }
 
+#-------------------------------------------------------------------------------
+# Update version numbers of AssemblyInfo.cs
+#-------------------------------------------------------------------------------
+function Update-AssemblyInfoFiles ([string] $version) {
+    $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $assemblyVersion = 'AssemblyVersion("' + $version + '")';
+    $fileVersion = 'AssemblyFileVersion("' + $version + '")';
+    
+    Get-ChildItem -r -filter AssemblyInfo.cs | ForEach-Object {
+        $filename = $_.Directory.ToString() + '\' + $_.Name
+        $filename + ' -> ' + $version
+        
+        # If you are using a source control that requires to check-out files before 
+        # modifying them, make sure to check-out the file here.
+        # For example, TFS will require the following command:
+        # tf checkout $filename
+    
+        (Get-Content $filename) | ForEach-Object {
+            % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
+            % {$_ -replace $fileVersionPattern, $fileVersion }
+        } | Set-Content $filename
+    }
+}
+
 function Get-ProgramFiles
 {
     #TODO: Someone please come up with a better way of detecting this - Tried http://msmvps.com/blogs/richardsiddaway/archive/2010/02/26/powershell-pack-mount-specialfolder.aspx and some enums missing
@@ -51,3 +81,4 @@ function Get-ProgramFiles
     }
     return "C:\Program Files"
 }
+
